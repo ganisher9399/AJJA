088b76a05581b24f13ce7f98f4ee20ca9760c6d7
G'anisher <95766585+ganisher9399@users.noreply.github.com>
2022-04-19 15:07:35 +0500

    first commit

---
+++ src/views/Home/components/Event.vue
@@ -0,0 +1,64 @@
+<template>
+   <section class="event">
+       <div class="container">
+           <Title :sub="subtitle" :title="title" />
+
+           <div class="btns-wrapper">
+                <button 
+                v-for="(item, idx) in collection"
+                :key="item.id"
+                class="btn"
+                :class="{ active: toButton === idx }"
+                @click="toButton = idx"
+                
+                
+                >
+                
+                 <span>  {{ item.ctg_name.text }}</span>
+                </button>
+           </div>
+
+
+           <div class="cards-wrapper">
+               <div 
+               class="card" 
+               v-for="item in collection[toButton].data"
+               :key="item.product_id"
+               
+               >
+                   <img :src="item.img" :alt="item.title">
+                   <h3 class="title">{{ item.title }}</h3>
+               </div>
+           </div>
+
+       </div>
+   </section>
+</template>
+
+
+<script>
+import Title from '../../../layouts/Title.vue'
+import Collection from '@/collection.js'
+
+export default {
+    components: {
+        Title,
+
+    },
+    data(){
+        return {
+          toButton: 0,  
+          subtitle: 'К мероприятиям',
+          title: 'Нацтояшая красота здеси!',
+          collection: Collection
+        }
+    }
+}
+</script>
+
+
+
+<style lang="scss">
+@import '../../../assets/styles/Pages/Home/Event-section.scss'
+    
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <MainSection />
+    <Event />
+  </div>
+</template>
+
+
+<script>
+
+import MainSection from './components/Main-section.vue'
+import Event from './components/Event.vue'
+
+export default {
+    components: {
+        MainSection,
+        Event,
+    }
+}
+</script>
+
+
+<style>
+    
+</style>
